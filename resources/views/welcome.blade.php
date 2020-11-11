3f3bd6687a76530e119f8c0355cf049adaf0bf4c
Diamé Morreau <diamemorreau@me.com>
2020-11-11 11:34:08 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,79 +3,78 @@
 @section('title','Team ADSD')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="card-columns">
-                {{-- Mounia --}}
-                <div class="card m-3" style="max-width: 540px;">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">
+<div class="container-fluid">
+    <div class="row">
+        <div class="card-columns">
+            {{-- Mounia --}}
+            <div class="card m-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
 
-                            <img src="{{ asset('images/mounia.png') }}" class="img-fluid img-thumbnail" alt="Card img">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Mounia Belmamoune</h5>
-                                <p class="card-text">
-                                    Team coordinator
-                                </p>
-                            </div>
+                        <img src="{{ asset('images/mounia.png') }}" class="img-fluid img-thumbnail" alt="Card img">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Mounia Belmamoune</h5>
+                            <p class="card-text">
+                                Team coordinator
+                            </p>
                         </div>
                     </div>
                 </div>
-                {{-- END Mounia --}}
+            </div>
+            {{-- END Mounia --}}
 
-                {{-- Stephan --}}
-                <div class="card m-3" style="max-width: 540px;">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">
-                            <a href="/stephan"><img src="{{ asset('images/sha.jpeg') }}" class="img-fluid img-thumbnail"
-                                                    alt="Card img"></a>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Stephan Hoekseam</h5>
-                                <p class="card-text">
-                                    Docent:
+            {{-- Stephan --}}
+            <div class="card m-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <a href="/stephan"><img src="{{ asset('images/sha.jpeg') }}" class="img-fluid img-thumbnail" alt="Card img"></a>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Stephan Hoekseam</h5>
+                            <p class="card-text">
+                                Docent:
                                 <ul>
                                     <li>DOMP</li>
                                     <li>Project WFFLIX</li>
                                     <li>Project AD</li>
                                     <li>Project LAB</li>
                                 </ul>
-                                </p>
-                            </div>
+                            </p>
                         </div>
                     </div>
                 </div>
-                {{-- END Stephan --}}
+            </div>
+            {{-- END Stephan --}}
 
-                {{-- Rudy --}}
-                <div class="card m-3" style="max-width: 540px;">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">
+            {{-- Rudy --}}
+            <div class="card m-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
 
-                            <img src="{{ asset('images/rudy.png') }}" class="img-fluid img-thumbnail" alt="Card img">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Rudy Borgstede</h5>
-                                <p class="card-text">
-                                    Docent:
+                        <img src="{{ asset('images/rudy.png') }}" class="img-fluid img-thumbnail" alt="Card img">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Rudy Borgstede</h5>
+                            <p class="card-text">
+                                Docent:
                                 <ul>
                                     <li>DOMP</li>
                                     <li>Project Flevosap</li>
                                     <li>Coach</li>
                                     <li>C# I & II</li>
                                 </ul>
-                                </p>
-                            </div>
+                            </p>
                         </div>
                     </div>
                 </div>
-                {{-- END Rudy --}}
-
             </div>
+            {{-- END Rudy --}}
+
         </div>
     </div>
-    @endsection
+</div>
+@endsection
