f2fe61fe66c34ef7ee5a319539f9f96299eb6081
Alex <AlexSteen303@gmail.com>
2020-11-16 08:49:37 +0100

    Kleine verandering migration. Meer accurate restricties

--- database/migrations/2020_11_14_162044_create_profiles_table.php
+++ database/migrations/2020_11_14_162044_create_profiles_table.php
@@ -14,12 +14,12 @@ class CreateProfilesTable extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->integer('user_id')->unsigned()->nullable();
+            $table->integer('user_id')->unsigned()->nullable(false);
             $table->string('name', 255)->collation('utf8mb4_unicode_ci')->nullable(false);
             $table->string('email', 255)->collation('utf8mb4_unicode_ci')->nullable(false);
             $table->text('about_me')->nullable();
             $table->string('team', 100)->nullable();
-            $table->string('team_role', 100)->nullable();
+            $table->string('team_role', 100)->nullable(false)->default('Developer');
             $table->string('projects', 100)->nullable();
             $table->timestamps();
         });
