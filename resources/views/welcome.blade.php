20c7f8401c54abdb117da022aac48d7d807bbb5e
lvvcolin <lorenzo8399@hotmail.com>
2020-11-23 22:04:20 +0100

    front welcome page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="wrapper" style="padding: 110px;">
+    <div class="container-fluid">
+        <div class="row flex-container">
+            <div class="col-md-5 vertical-middle">
+                <div class="inner" style="margin-top: 100px;">
+
+                    <h1 style="    font-size: 3.3rem;">The #1 software development tool used by agile teams</h1>
+
+                    <br>
+                    <button type="submit" class="btn btn-info">move to</button>
+
+
+                </div>
+            </div>
+            <div class="col-md-7">
+                <img style="width: 100%; height: 430px;" src="https://wac-cdn.atlassian.com/dam/jcr:b86a32cb-0aa8-4783-aa81-9592d4fbf829/jsw-header-illustrations---v3.png?cdnVersion=1342">
+            </div>
+        </div>
+
+    </div>
+</div>
+
+    @endsection
