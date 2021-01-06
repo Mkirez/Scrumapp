c1ee6c1ff7cc71b5f5cbc7719f9004aba07e559b
lvvcolin <lorenzo8399@hotmail.com>
2021-01-06 15:46:26 +0100

    project kan nu gezien worden door mensen die in het project worden gezet anders niet a

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -20,11 +20,30 @@ class ProjectController extends Controller
     public function index()
     {
 
+    $id = Auth::user()->id;
+    $rights=Auth::user()->rights;
+    
 
-    $project = Project::all();
+    if ($rights == '0'){
+        $sql = "SELECT projects.id, projects.name, projects.team_id, projects.created_at, projects.end_date, projects.start_date, projects.updated_at
+        FROM projects, team_users, users
+        WHERE projects.team_id = team_users.team_id and users.id=team_users.user_id AND users.id = '$id'";
+      $project = DB::select($sql);
+      //print_r($project);
+
+
+    }else{
+        $project = Project::all();
+    }
+       
+    //print_r(Project::all());
+   // exit;
+
+    
 
 
     return view('/projects')->with('projects', $project);
+    
 
 
 
