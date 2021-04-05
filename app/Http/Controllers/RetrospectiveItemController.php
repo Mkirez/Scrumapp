c1581cc5e1eb90448289f599f3a8d8d0e9959197
Diamé Morreau <diamemorreau@me.com>
2021-04-05 15:13:36 +0200

    geen hoofdletter bij Retrospective_item

--- app/Http/Controllers/RetrospectiveItemController.php
+++ app/Http/Controllers/RetrospectiveItemController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\retrospective_item;
+use App\Models\Retrospective_item;
 use App\Models\Project;
 use App\Models\Sprint;
 
